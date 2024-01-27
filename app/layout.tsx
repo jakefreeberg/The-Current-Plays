a52aedc2d1ba5a3a112f8d30a74e04df467b2637
Jake Freeberg <jtfreeberg@spscommerce.com>
2024-01-27 12:47:24 -0600

    polished up

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-indigo-200">{children}</body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,27 +8,27 @@ const config: Config = {
     './node_modules/react-tailwindcss-datepicker/dist/index.esm.js',
     './node_modules/react-tailwindcss-select/dist/index.esm.js',
   ],
-  theme: {
-    extend: {
-      gridTemplateColumns: {
-        '13': 'repeat(13, minmax(0, 1fr))',
-      },
-      colors: {
-        blue: {
-          400: '#2589FE',
-          500: '#0070F3',
-          600: '#2F6FEB',
-        },
-      },
-    },
-    keyframes: {
-      shimmer: {
-        '100%': {
-          transform: 'translateX(100%)',
-        },
-      },
-    },
-  },
+  // theme: {
+  //   extend: {
+  //     gridTemplateColumns: {
+  //       '13': 'repeat(13, minmax(0, 1fr))',
+  //     },
+  //     colors: {
+  //       blue: {
+  //         400: '#2589FE',
+  //         500: '#0070F3',
+  //         600: '#2F6FEB',
+  //       },
+  //     },
+  //   },
+  //   keyframes: {
+  //     shimmer: {
+  //       '100%': {
+  //         transform: 'translateX(100%)',
+  //       },
+  //     },
+  //   },
+  // },
   plugins: [require('@tailwindcss/forms')],
 };
 export default config;
